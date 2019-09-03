67e1c1299e1bd2ba04806c0262f476bb0a63da04
lejomer <lejomer14@gmail.com>
2019-09-03 18:50:55 -0500

    subir proyecto

---
+++ src/components/Title.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default class Title extends React.Component {
+    render(){
+        return(
+            <div className="titleText">
+                <h1>Places</h1>
+                <p>Descubre lugares <br></br>de manera simples</p>
+            </div>
+        );
+    }
+}
---
+++ src/requests/places.js
@@ -0,0 +1,19 @@
+export default {
+    places: [
+      {
+        imageUrl: '/images/place-1.jpg',
+        title: 'Starbucks Norte',
+        description: 'Starbucks Corporation is an American coffee company an coffeehouse'    
+      },
+      {
+        imageUrl: '/images/place-2.jpg',
+        title: 'Starbucks Norte',
+        description: 'Starbucks Corporation is an American coffee company an coffeehouse'   
+       },
+       {
+        imageUrl: '/images/place-3.jpg',
+        title: 'Pizza de Amor',
+        description: 'Starbucks Corporation is an American coffee company an coffeehouse'   
+       }  
+    ]
+}
